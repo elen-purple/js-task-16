50b1b5c29677314b797b39dfc3f68be302b14e05
elen-purple <anna.havrylova.2011@gmail.com>
2024-11-27 13:10:09 +0200

    Update task-4.js

--- js/task-4.js
+++ js/task-4.js
@@ -2,15 +2,15 @@ const StringBuilder = function (value) {
   this.value = value;
   this.append = function (str) {
     this.value += str;
-    return value;
+    return this.value;
   };
   this.prepend = function (str) {
     this.value = str + this.value;
-    return value;
+    return this.value;
   };
   this.pad = function (str) {
     this.value = str + this.value + str;
-    return value;
+    return this.value;
   };
 };
 const builder = new StringBuilder(".");
